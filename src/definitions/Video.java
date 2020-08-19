402a4cf6a7d1131d3360a14e070389dc70f02a48
Lenovo <garvit.gupta_cs19@gla.ac.in>
2020-08-19 23:33:13 +0530

    add fields for the Video.java class

--- src/definitions/Video.java
+++ src/definitions/Video.java
@@ -7,4 +7,15 @@
 package definitions;
 
 public class Video {
+
+    /*
+     * Creating Definiton Classes : (Rules)
+     * 1. Always set teh visibility modifier of a field to private unless otherwise specified.
+     * 2.Always set the access modifier of a field to non-static unless otherwise specified.
+     *
+     * */
+    // member variables == fields / variablesString
+    private String videoName;
+    private boolean checkOut;
+    private int rating;
 }
