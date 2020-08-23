b13b6f84916588b8f4d84ae9928a854779dcfc25
Lenovo <garvit.gupta_cs19@gla.ac.in>
2020-08-23 22:30:19 +0530

    create constructor for VideoStore.java

--- src/definitions/VideoStore.java
+++ src/definitions/VideoStore.java
@@ -9,6 +9,17 @@ package definitions;
 public class VideoStore {
     private Video[] store;
 
+    public VideoStore() {
+        this.store = new Video[10];
+        for (int i = 0; i < store.length; i++) {
+            store[i] = new Video();
+        }
+    }
+
+    public VideoStore(Video[] store) {
+        this.store = store;
+    }
+
     public Video[] getStore() {
         return store.clone();
     }
