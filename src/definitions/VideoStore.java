de9d63efe3686a4fa595988950996ee22bc5b28c
Lenovo <garvit.gupta_cs19@gla.ac.in>
2020-08-23 15:04:38 +0530

    create VideoStore.java

---
+++ src/definitions/VideoStore.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Garvit Gupta
+ *   Date: 23-08-2020
+ *   Time: 15:03
+ *   File: VideoStore.java
+ */
+package definitions;
+
+public class VideoStore {
+
+}
