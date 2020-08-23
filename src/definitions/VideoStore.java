1220c4be88d8522d81bc086f61bd302778cee6b3
Lenovo <garvit.gupta_cs19@gla.ac.in>
2020-08-23 22:33:41 +0530

    create the tostring(), equals() and the hashCode() methods

--- src/definitions/VideoStore.java
+++ src/definitions/VideoStore.java
@@ -6,6 +6,8 @@
  */
 package definitions;
 
+import java.util.Arrays;
+
 public class VideoStore {
     private Video[] store;
 
@@ -26,5 +28,23 @@ public class VideoStore {
 
     public void setStore(Video[] store) {
         this.store = store;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        VideoStore that = (VideoStore) o;
+        return Arrays.equals(getStore(), that.getStore());
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(getStore());
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(store);
     }
 }
