edb19da16ae48ee80565dfea6634482bfb69689c
Lenovo <garvit.gupta_cs19@gla.ac.in>
2020-08-23 22:51:10 +0530

    add a method to return a video

--- src/definitions/VideoStore.java
+++ src/definitions/VideoStore.java
@@ -67,4 +67,13 @@ public class VideoStore {
         System.out.println("Thank you for renting " + name + ".");
     }
 
+    /**
+     * This method returns a video back to the video rental store.
+     *
+     * @param name The name of the video you want to return.
+     */
+    public void doReturn(String name) {
+        System.out.println("Thank you for returning, " + name + ". Hope you liked it!");
+    }
+
 }
